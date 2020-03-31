552aa766d6ee972440d01a3538069c96e82a60e0
Mohammed Salah <themsm1922011@yahoo.com>
2020-03-31 14:31:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -27,13 +27,13 @@ function dateNow() {
 
 function GetApiData() {
   var recover = null;
-  $.getJSON('http://api.coronastatistics.live/all', function (res) {
+  $.getJSON('https://api.coronastatistics.live/all', function (res) {
     console.log(res);
     $("#confirmed").text(res.cases);
     $("#deaths").text(res.deaths);
     $("#recovered").text(res.recovered);
 
-    $.getJSON('http://api.coronastatistics.live/countries', function (data) {
+    $.getJSON('https://api.coronastatistics.live/countries', function (data) {
       for (let index = 0; index < data.length; index++) {
         var table = `<tr><td class="text-center">${data[index].country}</td><td class="text-center"> ${data[index].todayCases}</td> <td class="text-center">${data[index].cases} </td> <td class="text-center" style="color:red">${data[index].deaths} </td><td class="text-center" style="color:green">${data[index].recovered}</td><td class="text-center">${data[index].casesPerOneMillion}</td></tr>`
         $('#tblData tbody').append(table);
